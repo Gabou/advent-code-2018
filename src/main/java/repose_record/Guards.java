b4f36dd9816a7af54b3baaee92dedd72dbc57d8e
delaroch <gabriel.delarochette-ext@amundi.com>
2018-12-11 18:43:45 +0100

    finish day4-part1

--- src/main/java/repose_record/Guards.java
+++ src/main/java/repose_record/Guards.java
@@ -3,15 +3,14 @@ package repose_record;
 import reader.InputReader;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.Comparator;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Guards {
@@ -24,34 +23,72 @@ public class Guards {
         int actualGuard = 0;
 
         for (String string : objects) {
-            if(string.contains("shift")) {
-                actualGuard = Integer.valueOf(string.substring(string.indexOf("#")+1,string.indexOf(" begins")));
+            if (string.contains("shift")) {
+                actualGuard = Integer.valueOf(string.substring(string.indexOf('#') + 1, string.indexOf(" begins")));
                 if (stateByGuard.containsKey(actualGuard)) {
                     stateByGuard.get(actualGuard).add(string);
                 } else {
                     SortedSet<String> strings = new TreeSet<>();
                     strings.add(string);
-                    stateByGuard.put(actualGuard,strings);
+                    stateByGuard.put(actualGuard, strings);
                 }
             } else {
                 stateByGuard.get(actualGuard).add(string);
             }
         }
 
+        Map<Integer, Map<LocalTime, Integer>> frequencyOfSleepTimeByGard = new HashMap<>();
 
         for (Map.Entry<Integer, SortedSet<String>> integerSortedSetEntry : stateByGuard.entrySet()) {
-            System.out.println(integerSortedSetEntry.getKey());
+            frequencyOfSleepTimeByGard.put(integerSortedSetEntry.getKey(), new HashMap<>());
+            LocalTime sleepBegin = LocalTime.of(0, 0);
             for (String s : integerSortedSetEntry.getValue()) {
-                System.out.println("       " + s);
+                if (s.contains("asleep")) {
+                    sleepBegin = LocalTime.parse(s.substring(s.indexOf(' ') + 1, s.indexOf("] ")));
+                } else if (s.contains("wakes")) {
+                    LocalTime wakeUp = LocalTime.parse(s.substring(s.indexOf(' ') + 1, s.indexOf("] ")));
+
+                    while (!sleepBegin.equals(wakeUp)) {
+                        Integer time = frequencyOfSleepTimeByGard.get(integerSortedSetEntry.getKey()).putIfAbsent(sleepBegin, 1);
+                        if(time !=null) {
+                            frequencyOfSleepTimeByGard.get(integerSortedSetEntry.getKey()).put(sleepBegin, frequencyOfSleepTimeByGard.get(integerSortedSetEntry.getKey()).get(sleepBegin)+1);
+                        }
+                        sleepBegin = sleepBegin.plus(1, ChronoUnit.MINUTES);
+                    }
+                    sleepBegin = LocalTime.of(0,0);
+                }
             }
 
         }
 
-        /*
-        for (String object : objects) {
-            System.out.println(object);
+        int guardWithBiggestSleepTime = 0;
+        int sleepTime = 0;
+        LocalTime minuteWhenSleepingMost = LocalTime.of(0,0);
+
+        for (Map.Entry<Integer, Map<LocalTime, Integer>> integerMapEntry : frequencyOfSleepTimeByGard.entrySet()) {
+
+            LocalTime minuteWhenSleepingMostForThisGuard = LocalTime.of(0,0);
+            int sleepingTime = 0;
+            int moreFrequenciesOfSleep = 0;
+            for (Map.Entry<LocalTime, Integer> localTimeIntegerEntry : integerMapEntry.getValue().entrySet()) {
+                sleepingTime += localTimeIntegerEntry.getValue();
+                if (localTimeIntegerEntry.getValue() > moreFrequenciesOfSleep) {
+                    moreFrequenciesOfSleep = localTimeIntegerEntry.getValue();
+                    minuteWhenSleepingMostForThisGuard = localTimeIntegerEntry.getKey();
+                }
+            }
+
+            if(sleepingTime > sleepTime) {
+                guardWithBiggestSleepTime = integerMapEntry.getKey();
+                sleepTime = sleepingTime;
+                minuteWhenSleepingMost = minuteWhenSleepingMostForThisGuard;
+            }
         }
-        */
+
+        int result = guardWithBiggestSleepTime * minuteWhenSleepingMost.getMinute();
+
+        System.out.println(result);
+
     }
 
 }
