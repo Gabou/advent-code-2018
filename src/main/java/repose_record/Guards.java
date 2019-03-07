1b8bfd3f27288ccc2cf32a6ea70859640b8a2787
delaroch <gabriel.delarochette-ext@amundi.com>
2019-03-07 14:01:41 +0100

    Kata guard : refactoring  parser

--- src/main/java/repose_record/Guards.java
+++ src/main/java/repose_record/Guards.java
@@ -4,40 +4,24 @@ import reader.InputReader;
 
 import java.io.IOException;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class Guards {
 
     static GuardKataResult guardKata() throws IOException {
         List<String> input = InputReader.input("/inputDay4");
-        SortedSet<String> records = new TreeSet<>(input);
+        List<String> records = input.stream().sorted().collect(Collectors.toList());
 
-        Map<Integer, SortedSet<String>> stateByGuard = new HashMap<>();
-        int actualGuard = 0;
+        Map<Integer, List<String>> recordsByGuard = groupRecordByGuard(records);
 
-        for (String record : records) {
-            if (record.contains("shift")) {
-                actualGuard = parseGuardId(record);
-                if (!stateByGuard.containsKey(actualGuard)) {
-                    stateByGuard.put(actualGuard, new TreeSet<>());
-                }
-                stateByGuard.get(actualGuard).add(record);
-            } else {
-                stateByGuard.get(actualGuard).add(record);
-            }
-        }
-
-        Map<Integer, Map<LocalTime, Integer>> frequencyOfSleepTimeByGard = new HashMap<>();
-
-        for (Map.Entry<Integer, SortedSet<String>> integerSortedSetEntry : stateByGuard.entrySet()) {
-            Map<LocalTime, Integer> frequencyOfSleepTime = parseSleepRecords(integerSortedSetEntry.getValue());
-            frequencyOfSleepTimeByGard.put(integerSortedSetEntry.getKey(), frequencyOfSleepTime);
-        }
+        Map<Integer, Map<LocalTime, Integer>> frequencyOfSleepTimeByGard = mapValues(recordsByGuard, Guards::parseSleepRecords);
 
         int guardWithBiggestSleepTime = 0;
         int sleepTime = 0;
@@ -88,33 +72,51 @@ public class Guards {
         return new GuardKataResult(result1, result2);
     }
 
+    private static <K, V1, V2> Map<K, V2> mapValues(Map<K, V1> map, Function<V1, V2> valueMapper) {
+        return map.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> valueMapper.apply(entry.getValue())));
+    }
+
+    private static Map<Integer, List<String>> groupRecordByGuard(List<String> records) {
+        Map<Integer, List<String>> recordsByGuard = new HashMap<>();
+        int currentGuard = 0;
+
+        for (String record : records) {
+            if (record.contains("shift")) {
+                currentGuard = parseGuardId(record);
+                recordsByGuard.computeIfAbsent(currentGuard, i -> new ArrayList<>());
+            }
+            recordsByGuard.get(currentGuard).add(record);
+        }
+        return recordsByGuard;
+    }
+
     private static Integer parseGuardId(String record) {
         return Integer.valueOf(record.substring(record.indexOf('#') + 1, record.indexOf(" begins")));
     }
 
-    private static Map<LocalTime, Integer> parseSleepRecords(SortedSet<String> sleepRecordsForGuard) {
+    private static Map<LocalTime, Integer> parseSleepRecords(List<String> sleepRecordsForGuard) {
         Map<LocalTime, Integer> frequencyOfSleepTime = new HashMap<>();
-        LocalTime timeSleepBegin = LocalTime.of(0, 0);
+        LocalTime asleep = LocalTime.of(0, 0);
 
-        for (String s : sleepRecordsForGuard) {
-            if (s.contains("asleep")) {
-                timeSleepBegin = LocalTime.parse(s.substring(s.indexOf(' ') + 1, s.indexOf("] ")));
-            } else if (s.contains("wakes")) {
-                LocalTime wakeUp = LocalTime.parse(s.substring(s.indexOf(' ') + 1, s.indexOf("] ")));
+        for (String record : sleepRecordsForGuard) {
+            if (record.contains("asleep")) {
+                asleep = parseMinuteFromRecord(record);
+            } else if (record.contains("wakes")) {
+                LocalTime wakeUp = parseMinuteFromRecord(record);
 
-                while (!timeSleepBegin.equals(wakeUp)) {
-                    Integer time = frequencyOfSleepTime.putIfAbsent(timeSleepBegin, 1);
-                    if (time != null) {
-                        frequencyOfSleepTime.put(timeSleepBegin,
-                                frequencyOfSleepTime.get(timeSleepBegin) + 1);
-                    }
-                    timeSleepBegin = timeSleepBegin.plus(1, ChronoUnit.MINUTES);
+                for (LocalTime minute = asleep; minute.isBefore(wakeUp); minute = minute.plus(1, MINUTES)) {
+                    Integer newFrequency = frequencyOfSleepTime.getOrDefault(minute, 0) + 1;
+                    frequencyOfSleepTime.put(minute, newFrequency);
                 }
-                timeSleepBegin = LocalTime.of(0, 0);
             }
         }
 
         return frequencyOfSleepTime;
+    }
+
+    private static LocalTime parseMinuteFromRecord(String record) {
+        return LocalTime.parse(record.substring(record.indexOf(' ') + 1, record.indexOf("] ")));
     }
 
     public static void main(String[] args) throws IOException {
